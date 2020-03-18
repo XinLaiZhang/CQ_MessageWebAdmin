b857c51c9e177b7e218c38d6331a9d6d77eaccec
XinLaiZhang <XinLaiZhang2018@outlook.com>
2020-03-18 18:01:22 +0800

    添加首页

---
+++ src/main/webapp/js/index.js
@@ -0,0 +1,12 @@
+let player = new Player({
+  id: 'background',
+  url: './video/background',
+  autoplay: true,
+  autoplayMuted: true,
+  loop: true,
+  videoInit: true,
+  controls: false,
+  cssFullscreen: true,
+  fitVideoSize: 'fixWidth',
+  width: 1920
+});
