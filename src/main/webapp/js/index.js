e820602e7536fff068a8c9058c3bacb01ae9925c
XinLaiZhang <XinLaiZhang2018@outlook.com>
2020-03-19 09:12:41 +0800

    优化视频

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,12 +1,12 @@
 let player = new Player({
   id: 'background',
-  url: './video/background',
+  width: 1920,
+  url: 'https://cdn.jsdelivr.net/gh/XinLaiZhang/CQ_MessageWebAdmin@b857c51c9e177b7e218c38d6331a9d6d77eaccec/src/main/webapp/video/background',
   autoplay: true,
   autoplayMuted: true,
   loop: true,
   videoInit: true,
   controls: false,
   cssFullscreen: true,
-  fitVideoSize: 'fixWidth',
-  width: 1920
+  fitVideoSize: 'fixWidth'
 });
